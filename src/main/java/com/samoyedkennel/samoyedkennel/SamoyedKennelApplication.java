6e6c82145c87868d2ad7ebd0f5f80c31ccda8a76
razvanrmn <razvanrmn@icloud.com>
2024-01-27 16:03:48 +0200

    Initial commit

---
+++ src/main/java/com/samoyedkennel/samoyedkennel/SamoyedKennelApplication.java
@@ -0,0 +1,13 @@
+package com.samoyedkennel.samoyedkennel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamoyedKennelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamoyedKennelApplication.class, args);
+	}
+
+}
